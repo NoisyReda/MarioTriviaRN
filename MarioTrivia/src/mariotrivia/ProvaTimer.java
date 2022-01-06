15807839bef3644904a4d4fe5e3729254c1061c5
sciocotre <nespolimattia@gmail.com>
2022-01-06 18:24:33 +0100

    Seconda parte
    fix push errata stessa descrizione precedente

---
+++ MarioTrivia/src/mariotrivia/ProvaTimer.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mariotrivia;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+/**
+ *
+ * @author Mattia
+ */
+public class ProvaTimer extends Thread {
+
+    int secondimancanti = 10;
+
+    Timer myTimer = new Timer();
+    TimerTask task = new TimerTask() {
+        @Override
+        public void run() {
+            if (secondimancanti >= 1){
+                secondimancanti--;
+                System.out.println("Secondi mancanti:" + secondimancanti);
+            }
+        }
+    };
+
+    public void start() {
+        myTimer.scheduleAtFixedRate(task, 1000, 1000);
+    }
+}
